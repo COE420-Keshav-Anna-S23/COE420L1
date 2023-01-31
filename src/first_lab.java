0d3c77225f89f2680ec8d5ea1baad303861b0173
Anna <anna.ykbf@gmail.com>
2023-01-31 18:45:53 +0400

    Added feature B

--- src/first_lab.java
+++ src/first_lab.java
@@ -4,6 +4,13 @@ public class first_lab {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World :) ");
+		
+		featureB();
 	}
 
+	
+	public static void featureB()
+	{
+		System.out.println("works");
+	}
 }
